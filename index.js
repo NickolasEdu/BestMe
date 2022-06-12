73ffd7242daa7751690c72d96b0a05bc7b3cb30a
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-06-12 10:09:31 -0300

    App default features

---
+++ index.js
@@ -0,0 +1,25 @@
+const questions = [
+    "O que aprendi hoje?",
+    "O que me estressou hoje?",
+    "O que poderia ter feito melhor?",
+    "O que me fez feliz?",
+    "Quem eu ajudei hoje?",
+]
+
+const ask = (index = 0) => {
+    process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+ask()
+
+const anwsers = []
+
+process.stdin.on("data", data => {
+    anwsers.push(data.toString().trim())
+    if (anwsers.length < questions.length) {
+        ask(anwsers.length)
+    } else {
+        process.stdout.write(anwsers + "\n\n")
+        process.exit()
+    }
+})
